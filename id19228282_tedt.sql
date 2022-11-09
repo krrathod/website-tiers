1500fcb53063e514ac55447acda14ec7ed16c696
krrathod <68765891+krrathod@users.noreply.github.com>
2022-11-09 16:28:27 +0530

    Add files via upload

---
+++ id19228282_tedt.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 09, 2022 at 10:57 AM
+-- Server version: 10.5.16-MariaDB
+-- PHP Version: 7.3.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id19228282_tedt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AndroidInsert`
+--
+
+CREATE TABLE `AndroidInsert` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `pass` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `OTP` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `OTP_EXPIRE` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `AndroidInsert`
+--
+
+INSERT INTO `AndroidInsert` (`id`, `user`, `pass`, `OTP`, `OTP_EXPIRE`) VALUES
+(1, 'sad', 'sdsd', '1a8BD9z7a', '12:01 pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `heroes`
+--
+
+CREATE TABLE `heroes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `realname` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `rating` int(11) NOT NULL,
+  `teamaffiliation` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `heroes`
+--
+
+INSERT INTO `heroes` (`id`, `name`, `realname`, `rating`, `teamaffiliation`) VALUES
+(1, 'Iron Man', 'Tony', 5, 'Avanger'),
+(2, 'BatMan', 'Robert', 5, 'DC'),
+(3, 'Spiderman', 'Peter', 5, 'Avengers');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Status` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Offline',
+  `user_token` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `Status`, `user_token`) VALUES
+(1, 'Kartik', '123456', 'Offline', 'AYTVNp50tj');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `AndroidInsert`
+--
+ALTER TABLE `AndroidInsert`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `heroes`
+--
+ALTER TABLE `heroes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `AndroidInsert`
+--
+ALTER TABLE `AndroidInsert`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `heroes`
+--
+ALTER TABLE `heroes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
